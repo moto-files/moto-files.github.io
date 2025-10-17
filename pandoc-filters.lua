b940d0b3fd8fdd9c50ae55b03235be035bd33d42
EXL <exlmotodev@gmail.com>
2025-10-18 06:42:59 +0700

    Bump design

--- pandoc-filters.lua
+++ pandoc-filters.lua
@@ -1,5 +1,6 @@
 -- Adds anchor links to headings with IDs.
 -- See: https://github.com/jgm/pandoc-website/blob/master/tools/anchor-links.lua
+-- This will help disable visited links on headings and a proper anchors to it.
 function Header(header)
 	if header.identifier ~= '' then
 		-- Set header as link.
@@ -7,27 +8,32 @@ function Header(header)
 			header.content,                                           -- content
 			'#' .. header.identifier,                                 -- href
 			pandoc.utils.stringify(header.content),                   -- title
-			{ class = 'anchor-link', ['aria-hidden'] = 'true' }       -- attributes
+			{ class = 'anchor-link' }                                 -- attributes
 		)
 		header.content = { anchor_link }
-		return header
+		return pandoc.Div(
+			{ header }, -- Keep the header block as-is.
+			{ class = "pandoc-header" }
+		)
 	end
 end
 
 -- For wrapping inline code with a custom <span> tag.
+-- This will help add border/background to inline code without ugly CSS hacks.
 function Code(code)
 	-- Let Pandoc render the code as usual, then wrap it in a <span> tag.
 	return pandoc.Span(
 		{ code }, -- Keep the code as-is.
-		{class = "pandoc-code-inline"}
+		{ class = "pandoc-code-inline" }
 	)
 end
 
 -- For wrapping code blocks with a custom <div> tag.
+-- This will help add border/background to block code without ugly CSS hacks.
 function CodeBlock(code)
 	-- Let Pandoc render the block as usual, then wrap it in a <div> tag.
 	return pandoc.Div(
 		{ code }, -- Keep the code block as-is.
-		{class = "pandoc-code"}
+		{ class = "pandoc-code-block" }
 	)
 end
